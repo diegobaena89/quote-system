806d79c05f500e9b6a4ce0ee4117824c98128442
diegobaena89 <diegobf.cello@gmail.com>
2021-10-27 23:35:25 -0300

    adding home handlebars

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const conn = require('./db/conn');
 const Tought = require('./models/Tought');
 const User = require('./models/User');
 
+// Import Routes
+const toughtRoutes = require('./routes/toughtsRoutes');
+
+// Import Controller
+const ToughtController = require('./controllers/ToughtController');
+
 // middlewares
 app.engine('handlebars', exhbs());
 app.set('view engine', 'handlebars');
@@ -59,6 +65,11 @@ app.use((req, res, next) => {
 
   next();
 });
+
+// Routes
+app.use('/toughts', toughtRoutes);
+
+app.get('/', ToughtController.showToughts);
 
 conn
   // .sync({ force: true })
