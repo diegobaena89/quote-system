24e34bee37c70a2413067908bc94f27579571004
diegobaena89 <diegobf.cello@gmail.com>
2021-11-03 21:21:04 -0300

    auth middleware

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ToughtController = require('../controllers/ToughtController');
-// controller
 
+// helper
+const checkAuth = require('../helpers/auth').checkAuth;
+
+// controller
+router.get('/dashboard', checkAuth, ToughtController.dashboard);
 router.get('/', ToughtController.showToughts);
 
 module.exports = router;
