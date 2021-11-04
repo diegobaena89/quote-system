a540c8ebaf67452fedea3e29eab25ad791106c3c
diegobaena89 <diegobf.cello@gmail.com>
2021-11-03 21:40:16 -0300

    Inserting data

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -9,4 +9,27 @@ module.exports = class ToughtController {
   static async dashboard(req, res) {
     res.render('toughts/dashboard');
   }
+
+  static createTought(req, res) {
+    res.render('toughts/create');
+  }
+
+  static async createToughtSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      userId: req.session.userid,
+    };
+
+    try {
+      await Tought.create(tought);
+
+      req.flash('message', 'Pensamento criado com sucesso!');
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
